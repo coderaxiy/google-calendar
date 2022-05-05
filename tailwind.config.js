f0327304a36cb635483a063f15e567312ff1b145
AhmadjanovD <halilullahahmadjanov@gmail.com>
2022-05-05 14:49:54 +0900

    google-calendar-clone

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+const labelsClasses = [
+  "indigo",
+  "gray",
+  "green",
+  "blue",
+  "red",
+  "purple",
+];
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  safelist: [
+    ...labelsClasses.map((lbl) => `bg-${lbl}-500`),
+    ...labelsClasses.map((lbl) => `bg-${lbl}-200`),
+    ...labelsClasses.map((lbl) => `text-${lbl}-400`)
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans"]
+      },
+      gridTemplateColumns: {
+        "1/5": "1fr 5fr"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+}
